a57f3fe802306e3edfa21d52e407bfe955ff1a55
tarper24 <tarper24@tarper24.net>
2023-01-27 00:53:42 -0600

    ✨ CF Worker for Backblaze B2 S3 files

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import { AwsClient } from 'aws4fetch'
+
+const aws = new AwsClient({
+	"accessKeyId": AWS_ACCESS_KEY_ID,
+	"secretAccessKey": AWS_SECRET_ACCESS_KEY,
+	"region": AWS_DEFAULT_REGION
+});
+
+addEventListener('fetch', function (event) {
+	event.respondWith(handleRequest(event.request))
+});
+
+async function handleRequest(request) {
+	let url = new URL(request.url);
+	url.hostname = AWS_S3_BUCKET;
+	let signedRequest = await aws.sign(url);
+	return await fetch(signedRequest, { "cf": { "cacheEverything": true } });
+}
